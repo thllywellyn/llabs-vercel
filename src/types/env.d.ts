147c18f4caf6de2a75cf5fa6b378da293bb24c9d
Llywellyn Sana <thllywellyn@gmail.com>
2025-05-06 00:23:03 +0530

    conversion finalisation and linking

--- src/types/env.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    SMTP_HOST: string;
-    SMTP_PORT: string;
-    SMTP_USER: string;
-    SMTP_PASS: string;
-    CONTACT_EMAIL: string;
-  }
-}
